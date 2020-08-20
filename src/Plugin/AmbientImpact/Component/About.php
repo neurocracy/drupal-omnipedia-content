a4f4e7ec6444e70648db4e62be742bf295c0ef62
Matei Stanca <i@ambientimpact.com>
2020-08-20 14:10:44 -0400

    Added <about> OmnipediaElement.

---
+++ src/Plugin/AmbientImpact/Component/About.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\omnipedia_content\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Omnipedia <about> element component.
+ *
+ * @Component(
+ *   id = "about",
+ *   title = @Translation("Omnipedia &lt;about&gt; element"),
+ *   description = @Translation("Loosely based on the <a href='https://en.wikipedia.org/wiki/Template:About'>Wikipedia Template:About</a>.")
+ * )
+ */
+class About extends ComponentBase {
+}
---
+++ src/Plugin/Omnipedia/Element/About.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Drupal\omnipedia_content\Plugin\Omnipedia\Element;
+
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\omnipedia_content\OmnipediaElementBase;
+use Drupal\omnipedia_core\Service\TimelineInterface;
+use Drupal\omnipedia_core\Service\WikiNodeRevisionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * About element.
+ *
+ * @OmnipediaElement(
+ *   id = "about",
+ *   html_element = "about",
+ *   title = @Translation("About"),
+ *   description = @Translation("Loosely based on the <a href='https://en.wikipedia.org/wiki/Template:About'>Wikipedia Template:About</a>.")
+ * )
+ */
+class About extends OmnipediaElementBase {
+
+  /**
+   * The Omnipedia timeline service.
+   *
+   * @var \Drupal\omnipedia_core\Service\TimelineInterface
+   */
+  protected $timeline;
+
+  /**
+   * The Omnipedia wiki node revision service.
+   *
+   * @var \Drupal\omnipedia_core\Service\WikiNodeRevisionInterface
+   */
+  protected $wikiNodeRevision;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Drupal\omnipedia_core\Service\TimelineInterface $timeline
+   *   The Omnipedia timeline service.
+   *
+   * @param \Drupal\omnipedia_core\Service\WikiNodeRevisionInterface $wikiNodeRevision
+   *   The Omnipedia wiki node revision service.
+   */
+  public function __construct(
+    array $configuration, string $pluginID, array $pluginDefinition,
+    TranslationInterface      $stringTranslation,
+    TimelineInterface         $timeline,
+    WikiNodeRevisionInterface $wikiNodeRevision
+  ) {
+    parent::__construct(
+      $configuration, $pluginID, $pluginDefinition, $stringTranslation
+    );
+
+    // Save dependencies.
+    $this->timeline         = $timeline;
+    $this->wikiNodeRevision = $wikiNodeRevision;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration, $pluginID, $pluginDefinition
+  ) {
+    return new static(
+      $configuration, $pluginID, $pluginDefinition,
+      $container->get('string_translation'),
+      $container->get('omnipedia.timeline'),
+      $container->get('omnipedia.wiki_node_revision')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getTheme(): array {
+    return [
+      'variables' => [
+        'about'     => '',
+        'uses'      => [],
+      ],
+      'template'  => 'about',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRenderArray(): array {
+    /** @var array */
+    $uses = [];
+
+    /** @var \DOMElement|null */
+    $element = $this->elements->getNode(0);
+
+    /** @var \DOMNamedNodeMap */
+    $attributes = $element->attributes;
+
+    foreach ($attributes as $attribute) {
+      // Determine if this is a numbered 'use' attribute, skipping if not.
+      \preg_match('%use(\d+)%', $attribute->name, $optionNameMatches);
+
+      if (!isset($optionNameMatches[1])) {
+        continue;
+      }
+
+      $optionIndex = $optionNameMatches[1];
+
+      // Set an error if this numbered 'use' attribute doesn't have a
+      // corresponding 'see' attribute.
+      if (!$element->hasAttribute('see' . $optionIndex)) {
+        $this->setError($this->t(
+          'You need to include a <code>@see</code> option along with the <code>@use</code> option.',
+          [
+            '@see'  => 'see' . $optionIndex,
+            '@use'  => 'use' . $optionIndex,
+          ]
+        ));
+
+        continue;
+      }
+
+      $seeNodeTitle = $element->getAttribute('see' . $optionIndex);
+
+      /** @var \Drupal\omnipedia_core\Entity\NodeInterface|null */
+      $seeNode = $this->wikiNodeRevision->getWikiNodeRevision(
+        $seeNodeTitle,
+        $this->timeline->getDateFormatted('current', 'storage')
+      );
+
+      if ($seeNode === null) {
+        $this->setError($this->t(
+          '<code>@attribute</code> attribute: Cannot find the specified wiki page titled "@title" in the current date.',
+          [
+            '@attribute'  => 'see' . $optionIndex,
+            '@title'      => $seeNodeTitle,
+          ]
+        ));
+
+        continue;
+      }
+
+      // If both matching 'use' and 'see' attributes are found, save them to the
+      // array to pass to the template.
+      $uses[] = [
+        'use' => $attribute->value,
+        'see' => [
+          '#type'   => 'link',
+          '#title'  => $seeNodeTitle,
+          '#url'    => $seeNode->toUrl(),
+        ],
+      ];
+    }
+
+    return [
+      '#theme'  => 'about',
+      // Render any HTML elements nested inside the <about> element.
+      '#about'  => ['#markup' => $this->elements->html()],
+      '#uses'   => $uses,
+
+      '#attached'   => [
+        'library'   => ['omnipedia_content/component.about'],
+      ],
+    ];
+  }
+
+}
