5acaea1074187b9fb6fa20ed8a3158069558ecbb
Matei Stanca <i@ambientimpact.com>
2021-03-25 15:57:06 -0400

    WikiNodeChangesInterface::build(): added note about no access checking.

--- src/Service/WikiNodeChangesInterface.php
+++ src/Service/WikiNodeChangesInterface.php
@@ -23,6 +23,10 @@ interface WikiNodeChangesInterface {
    * generates the diff via the HTML Diff service that the Diff module provides,
    * and alters/adjust the output as follows before returning it.
    *
+   * Note that this doesn't do any access checking, so code that calls this is
+   * responsible for not displaying information about nodes the user does not
+   * have access to.
+   *
    * @param \Drupal\omnipedia_core\Entity\NodeInterface $node
    *   A node object.
    *
