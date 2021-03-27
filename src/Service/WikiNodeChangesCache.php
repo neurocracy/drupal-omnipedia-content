09c80d3a9e78c6d32df85e8f666e679efb385fab
Matei Stanca <i@ambientimpact.com>
2021-03-27 15:13:28 -0400

    Wiki node changes cache service now uses wiki changes info service.

--- src/Service/WikiNodeChangesCache.php
+++ src/Service/WikiNodeChangesCache.php
@@ -3,8 +3,8 @@
 namespace Drupal\omnipedia_content\Service;
 
 use Drupal\Core\Cache\CacheBackendInterface;
-use Drupal\Core\Cache\Context\CacheContextsManager;
 use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\omnipedia_content\Service\WikiNodeChangesInfoInterface;
 use Drupal\omnipedia_content\Service\WikiNodeChangesCacheInterface;
 use Drupal\omnipedia_core\Entity\NodeInterface;
 
@@ -18,13 +18,6 @@ use Drupal\omnipedia_core\Entity\NodeInterface;
 class WikiNodeChangesCache implements WikiNodeChangesCacheInterface {
 
   /**
-   * The Drupal cache contexts manager.
-   *
-   * @var \Drupal\Core\Cache\Context\CacheContextsManager
-   */
-  protected $cacheContextsManager;
-
-  /**
    * The Omnipedia wiki node changes cache bin.
    *
    * @var \Drupal\Core\Cache\CacheBackendInterface
@@ -32,59 +25,29 @@ class WikiNodeChangesCache implements WikiNodeChangesCacheInterface {
   protected $changesCache;
 
   /**
-   * Constructs this service object.
+   * The Omnipedia wiki node changes info service.
    *
-   * @param \Drupal\Core\Cache\Context\CacheContextsManager $cacheContextsManager
-   *   The Drupal cache contexts manager.
+   * @var \Drupal\omnipedia_content\Service\WikiNodeChangesInfoInterface
+   */
+  protected $wikiNodeChangesInfo;
+
+  /**
+   * Constructs this service object.
    *
    * @param \Drupal\Core\Cache\CacheBackendInterface $changesCache
    *   The Omnipedia wiki node changes cache bin.
+   *
+   * @param \Drupal\omnipedia_content\Service\WikiNodeChangesInfoInterface $wikiNodeChangesInfo
+   *   The Omnipedia wiki node changes info service.
    */
   public function __construct(
-    CacheContextsManager  $cacheContextsManager,
-    CacheBackendInterface $changesCache
+    CacheBackendInterface         $changesCache,
+    WikiNodeChangesInfoInterface  $wikiNodeChangesInfo
   ) {
 
     // Save dependencies.
-    $this->cacheContextsManager = $cacheContextsManager;
     $this->changesCache         = $changesCache;
-
-  }
-
-  /**
-   * Create the cache ID for a provided wiki node.
-   *
-   * Note that this only generates variations based the node ID (nid), language,
-   * negotiated theme, and the current user's permissions hash. A more advanced
-   * version of this could replicate what the render cache does, but we don't
-   * anticipate requiring that level of granular variation; this is a compromise
-   * between having enough variations so that we don't expose any admin stuff
-   * (e.g. contextual links) or create security issues, and avoiding caching
-   * more variations than we expect to need for the sake of performance.
-   *
-   * @param \Drupal\omnipedia_core\Entity\NodeInterface $node
-   *   The wiki node to create the cache ID for.
-   *
-   * @return string
-   *   The cache ID.
-   *
-   * @see \Drupal\Core\Render\RenderCache::createCacheID()
-   *   Drupal core render cache ID generation for reference.
-   */
-  protected function createCacheID(NodeInterface $node): string {
-
-    // Build the cache keys to vary by based on these hard-coded cache contexts.
-    // The cache contexts manager will automatically populate these with the
-    // relevant values, e.g. the language code or current user's permissions
-    // hash.
-    /** @var array */
-    $cacheKeys = $this->cacheContextsManager->convertTokensToKeys([
-      'languages:language_interface',
-      'theme',
-      'user.permissions',
-    ])->getKeys();
-
-    return $node->nid->getString() . ':' . \implode(':', $cacheKeys);
+    $this->wikiNodeChangesInfo  = $wikiNodeChangesInfo;
 
   }
 
@@ -92,7 +55,9 @@ class WikiNodeChangesCache implements WikiNodeChangesCacheInterface {
    * {@inheritdoc}
    */
   public function isCached(NodeInterface $node): bool {
-    return \is_object($this->changesCache->get($this->createCacheID($node)));
+    return \is_object($this->changesCache->get(
+      $this->wikiNodeChangesInfo->getCacheId($node)
+    ));
   }
 
   /**
@@ -101,7 +66,9 @@ class WikiNodeChangesCache implements WikiNodeChangesCacheInterface {
   public function get(NodeInterface $node): ?array {
 
     if ($this->isCached($node)) {
-      return $this->changesCache->get($this->createCacheID($node))->data;
+      return $this->changesCache->get(
+        $this->wikiNodeChangesInfo->getCacheId($node)
+      )->data;
     }
 
     return null;
@@ -119,7 +86,8 @@ class WikiNodeChangesCache implements WikiNodeChangesCacheInterface {
     );
 
     $this->changesCache->set(
-      $this->createCacheID($node), $renderArray,
+      $this->wikiNodeChangesInfo->getCacheId($node),
+      $renderArray,
       $bubbleableMetadata->getCacheMaxAge(),
       $bubbleableMetadata->getCacheTags()
     );
