0e30a993ef5e1b65b37d7477df865ec10d424ec1
Matei Stanca <i@ambientimpact.com>
2021-04-06 22:13:40 -0400

    OmnipediaWikiNodeChangesController now logs uncached views.

--- omnipedia_content_changes/src/Controller/OmnipediaWikiNodeChangesController.php
+++ omnipedia_content_changes/src/Controller/OmnipediaWikiNodeChangesController.php
@@ -6,17 +6,35 @@ use Drupal\Component\Utility\Xss;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Logger\RfcLogLevel;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\omnipedia_content_changes\Service\WikiNodeChangesBuilderInterface;
 use Drupal\omnipedia_content_changes\Service\WikiNodeChangesCacheInterface;
+use Drupal\omnipedia_content_changes\Service\WikiNodeChangesInfoInterface;
+use Drupal\omnipedia_content_changes\Service\WikiNodeChangesUserInterface;
 use Drupal\omnipedia_core\Entity\NodeInterface;
 use Drupal\omnipedia_core\Service\TimelineInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for the Omnipedia wiki node changes route.
  */
 class OmnipediaWikiNodeChangesController extends ControllerBase {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Our logger channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $loggerChannel;
 
   /**
    * The Omnipedia timeline service.
@@ -40,7 +58,20 @@ class OmnipediaWikiNodeChangesController extends ControllerBase {
   protected $wikiNodeChangesCache;
 
   /**
+   * The Omnipedia wiki node changes info service.
+   *
+   * @var \Drupal\omnipedia_content_changes\Service\WikiNodeChangesInfoInterface
+   */
+  protected $wikiNodeChangesInfo;
+
+  /**
    * Constructs this controller; saves dependencies.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $currentUser
+   *   The current user.
+   *
+   * @param \Psr\Log\LoggerInterface $loggerChannel
+   *   Our logger channel.
    *
    * @param \Drupal\omnipedia_core\Service\TimelineInterface $timeline
    *   The Omnipedia timeline service.
@@ -50,15 +81,31 @@ class OmnipediaWikiNodeChangesController extends ControllerBase {
    *
    * @param \Drupal\omnipedia_content_changes\Service\WikiNodeChangesCacheInterface $wikiNodeChangesCache
    *   The Omnipedia wiki node changes cache service.
+   *
+   * @param \Drupal\omnipedia_content_changes\Service\WikiNodeChangesInfoInterface $wikiNodeChangesInfo
+   *   The Omnipedia wiki node changes info service.
+   *
+   * @param \Drupal\omnipedia_content_changes\Service\WikiNodeChangesUserInterface $wikiNodeChangesUser
+   *   The Omnipedia wiki node changes user service.
    */
   public function __construct(
+    AccountInterface                $currentUser,
+    LoggerInterface                 $loggerChannel,
     TimelineInterface               $timeline,
     WikiNodeChangesBuilderInterface $wikiNodeChangesBuilder,
-    WikiNodeChangesCacheInterface   $wikiNodeChangesCache
+    WikiNodeChangesCacheInterface   $wikiNodeChangesCache,
+    WikiNodeChangesInfoInterface    $wikiNodeChangesInfo,
+    WikiNodeChangesUserInterface    $wikiNodeChangesUser
   ) {
+
+    $this->currentUser            = $currentUser;
+    $this->loggerChannel          = $loggerChannel;
     $this->timeline               = $timeline;
     $this->wikiNodeChangesBuilder = $wikiNodeChangesBuilder;
     $this->wikiNodeChangesCache   = $wikiNodeChangesCache;
+    $this->wikiNodeChangesInfo    = $wikiNodeChangesInfo;
+    $this->wikiNodeChangesUser    = $wikiNodeChangesUser;
+
   }
 
   /**
@@ -66,9 +113,13 @@ class OmnipediaWikiNodeChangesController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
+      $container->get('current_user'),
+      $container->get('logger.factory')->get('omnipedia_content_changes'),
       $container->get('omnipedia.timeline'),
       $container->get('omnipedia.wiki_node_changes_builder'),
-      $container->get('omnipedia.wiki_node_changes_cache')
+      $container->get('omnipedia.wiki_node_changes_cache'),
+      $container->get('omnipedia.wiki_node_changes_info'),
+      $container->get('omnipedia.wiki_node_changes_user')
     );
   }
 
@@ -144,7 +195,27 @@ class OmnipediaWikiNodeChangesController extends ControllerBase {
   public function view(NodeInterface $node): array {
 
     if (!$this->wikiNodeChangesCache->isCached($node)) {
+
+      // Log this uncached view attempt in case it's useful data for debugging
+      // or future optimizations.
+      $this->loggerChannel->log(
+        RfcLogLevel::DEBUG,
+        'Wiki node changes not cached: user <code>%uid</code> requested node <code>%nid</code> with cache ID <code>%cid</code><br>Available cache IDs for this node:<pre>%cids</pre>Current user\'s roles:<pre>%roles</pre>',
+        [
+          '%uid'    => $this->currentUser->id(),
+          '%nid'    => $node->nid->getString(),
+          '%cid'    => $this->wikiNodeChangesInfo->getCacheId(
+            $node->nid->getString()
+          ),
+          '%cids'   => \print_r($this->wikiNodeChangesInfo->getCacheIds(
+            $node->nid->getString()
+          ), true),
+          '%roles'  => \print_r($this->currentUser->getRoles(), true),
+        ]
+      );
+
       return $this->wikiNodeChangesBuilder->buildPlaceholder($node);
+
     }
 
     return $this->wikiNodeChangesBuilder->build($node);
