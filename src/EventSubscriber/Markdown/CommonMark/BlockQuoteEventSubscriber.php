905182d6885e054e6d8dcff89f96a41c6e370ab2
Matei Stanca <i@ambientimpact.com>
2021-05-05 04:19:28 -0400

    CommonMark BlockQuote elements that are immediate siblings now merged.

--- src/EventSubscriber/Markdown/CommonMark/BlockQuoteEventSubscriber.php
+++ src/EventSubscriber/Markdown/CommonMark/BlockQuoteEventSubscriber.php
@@ -3,7 +3,9 @@
 namespace Drupal\omnipedia_content\EventSubscriber\Markdown\CommonMark;
 
 use Drupal\ambientimpact_markdown\AmbientImpactMarkdownEventInterface;
+use Drupal\ambientimpact_markdown\Event\Markdown\CommonMark\DocumentParsedEvent;
 use Drupal\ambientimpact_markdown\Event\Markdown\CommonMark\DocumentPreParsedEvent;
+use League\CommonMark\Block\Element\BlockQuote;
 use League\CommonMark\Input\MarkdownInput;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -17,7 +19,10 @@ class BlockQuoteEventSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents(): array {
     return [
-      AmbientImpactMarkdownEventInterface::COMMONMARK_DOCUMENT_PRE_PARSED => 'onCommonMarkDocumentPreParsed',
+      AmbientImpactMarkdownEventInterface::COMMONMARK_DOCUMENT_PRE_PARSED =>
+        'onCommonMarkDocumentPreParsed',
+      AmbientImpactMarkdownEventInterface::COMMONMARK_DOCUMENT_PARSED =>
+        'onCommonMarkDocumentParsed',
     ];
   }
 
@@ -46,6 +51,74 @@ class BlockQuoteEventSubscriber implements EventSubscriberInterface {
     }
 
     $event->setMarkdown(new MarkdownInput(\implode("\n", $lines)));
+
+  }
+
+  /**
+   * DocumentParsedEvent callback.
+   *
+   * This merges BlockQuote elements that are immediate siblings.
+   *
+   * @param \Drupal\ambientimpact_markdown\Event\Markdown\CommonMark\DocumentParsedEvent $event
+   *   The event object.
+   */
+  public function onCommonMarkDocumentParsed(DocumentParsedEvent $event): void {
+
+    /** @var \League\CommonMark\Block\Element\Document */
+    $document = $event->getDocument();
+
+    /** @var \League\CommonMark\Node\NodeWalker */
+    $walker = $document->walker();
+
+    /** @var \League\CommonMark\Block\Element\BlockQuote[] */
+    $startBlockQuotes = [];
+
+    while ($event = $walker->next()) {
+
+      /** @var \League\CommonMark\Node\Node */
+      $node = $event->getNode();
+
+      // Save any BlockQuote elements that are not preceded by another
+      // BlockQuote but do have a next sibling that is a BlockQuote.
+      if (
+        $node instanceof BlockQuote && $event->isEntering() &&
+        !($node->previous() instanceof BlockQuote) &&
+        $node->next() instanceof BlockQuote
+      ) {
+        $startBlockQuotes[] = $node;
+      }
+
+    }
+
+    foreach ($startBlockQuotes as $startBlockQuote) {
+
+      $currentNode = $startBlockQuote;
+
+      /** @var \League\CommonMark\Block\Element\BlockQuote[] */
+      $emptyBlockQuotes = [];
+
+      while (
+        $currentNode->next() instanceof BlockQuote &&
+        $currentNode = $currentNode->next()
+      ) {
+
+        // Append all children of this BlockQuote to the BlockQuote at the start
+        // of the sequence.
+        foreach ($currentNode->children() as $child) {
+          $startBlockQuote->appendChild($child);
+        }
+
+        // Save the now-empty BlockQuote.
+        $emptyBlockQuotes[] = $currentNode;
+
+      }
+
+      // Finally, remove the empty BlockQuotes.
+      foreach ($emptyBlockQuotes as $removeNode) {
+        $removeNode->detach();
+      }
+
+    }
 
   }
 
