25859f1980e11909eafcf1b06731e5c9c802ceea
Matei Stanca <i@ambientimpact.com>
2021-08-23 09:07:37 -0400

    DiffPrepareEventSubscriber: fixed incorrect list nesting.

--- omnipedia_content_changes/src/EventSubscriber/Omnipedia/Changes/DiffPrepareEventSubscriber.php
+++ omnipedia_content_changes/src/EventSubscriber/Omnipedia/Changes/DiffPrepareEventSubscriber.php
@@ -67,6 +67,71 @@ class DiffPrepareEventSubscriber implements EventSubscriberInterface {
   }
 
   /**
+   * Fixes any invalid nesting of a list inside of an <ins> or <del>.
+   *
+   * @param \Drupal\omnipedia_content_changes\Event\Omnipedia\Changes\DiffPostBuildEvent $event
+   *   The event object.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Content_categories#transparent_content_model
+   *   <ins> and <del> elements only permit transparent content, which does not
+   *   include lists, and so results in browsers interrupting the <ins> and
+   *   <del> to render the list and then continuing the <ins> or <del> after the
+   *   list.
+   */
+  protected function fixDiffListNesting(DiffPostBuildEvent $event): void {
+
+    /** @var \Symfony\Component\DomCrawler\Crawler */
+    $crawler = $event->getCrawler();
+
+    $listXpath = '[(.//ol or .//ul or .//dl)]';
+
+    // Find all <ins> and <del> elements that contain a list.
+    foreach ($crawler->evaluate(
+      '//ins' . $listXpath . '|//del' . $listXpath
+    ) as $element) {
+
+      /** @var \Symfony\Component\DomCrawler\Crawler */
+      $textCrawler = (new Crawler($element))->evaluate('.//text()');
+
+      foreach ($textCrawler as $textNode) {
+
+        // Skip text nodes that contain only white-space. This avoids adding
+        // unnecessary highlighting and also skips text nodes that are direct
+        // children of a list, which shouldn't be wrapped in an <ins> or <del>
+        // as it would be invalid nesting.
+        if (\mb_strlen(\trim($textNode->textContent)) === 0) {
+          continue;
+        }
+
+        /** @var \DOMElement|false */
+        $cloned = $element->cloneNode();
+
+        // Skip this text node if we couldn't clone the descendent <ins>/<del>,
+        // or if the cloned <ins>/<del> could not be inserted after the text
+        // node.
+        if (
+          !\is_object($cloned) ||
+          !\is_object(
+            $textNode->parentNode->insertBefore($cloned, $textNode)
+          )
+        ) {
+          continue;
+        }
+
+        // Finally, append the text node to the cloned <ins>/<del>.
+        $cloned->appendChild($textNode);
+
+      }
+
+      // Once the text nodes have been wrapped in clones of $element, unwrap
+      // $element.
+      Html::unwrapNode($element);
+
+    }
+
+  }
+
+  /**
    * DiffPostBuildEvent handler.
    *
    * @param \Drupal\omnipedia_content_changes\Event\Omnipedia\Changes\DiffPostBuildEvent $event
@@ -75,6 +140,8 @@ class DiffPrepareEventSubscriber implements EventSubscriberInterface {
   public function onDiffPostBuild(DiffPostBuildEvent $event): void {
 
     $this->unwrapNestedDiffElements($event);
+
+    $this->fixDiffListNesting($event);
 
   }
 
