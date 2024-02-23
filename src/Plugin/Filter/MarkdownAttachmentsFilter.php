9f18ddecb058cd190698624bd8b12a09beef8c1f
Matei Stanca <i@ambientimpact.com>
2024-02-23 18:33:04 -0500

    MarkdownAttachmentsFilter: Removed conditional logic:
    This fixes instances where aggregated assets could be created missing
    the references JavaScript if it was created on a wiki page that didn't
    have references, i.e. it didn't vary correctly since we never
    implemented a cache context for that. We can reintroduce the conditional
    logic at a later date if we can correctly vary the output as mentioned
    above.

--- src/Plugin/Filter/MarkdownAttachmentsFilter.php
+++ src/Plugin/Filter/MarkdownAttachmentsFilter.php
@@ -6,7 +6,6 @@ namespace Drupal\omnipedia_content\Plugin\Filter;
 
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
-use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Provides a filter to add Markdown attachments (libraries and settings).
@@ -14,7 +13,7 @@ use Symfony\Component\DomCrawler\Crawler;
  * @Filter(
  *   id           = "omnipedia_markdown_attachments",
  *   title        = @Translation("Omnipedia: Markdown libraries and settings"),
- *   description  = @Translation("This attaches libraries and JavaScript settings to the filter results if they contain Markdown elements, such as table of contents and references (citations, footnotes). This should be placed <strong>after</strong> the Markdown filter in the processing order."),
+ *   description  = @Translation("This attaches libraries to the filter results for our Markdown elements such as abbreviations and references (citations, footnotes). This should be placed <strong>after</strong> the Markdown filter in the processing order."),
  *   type         = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE
  * )
  */
@@ -24,37 +23,25 @@ class MarkdownAttachmentsFilter extends FilterBase {
    * {@inheritdoc}
    */
   public function process($text, $langCode) {
+
     /** @var \Drupal\filter\FilterProcessResult */
     $result = new FilterProcessResult($text);
 
-    /** @var \Symfony\Component\DomCrawler\Crawler */
-    $crawler = new Crawler((string) $text);
-
     foreach ([
-      [
-        'selector'    => 'abbr',
-        'attachments' => [
-          'library' => ['ambientimpact_ux/component.abbr'],
-        ],
+      'abbr' => [
+        'library' => ['ambientimpact_ux/component.abbr'],
       ],
-      [
-        'selector'    => '.references',
-        'attachments' => [
-          'library' => ['omnipedia_content/component.reference'],
-        ],
+      '.references' => [
+        'library' => ['omnipedia_content/component.reference'],
       ],
-    ] as $group) {
-      /** @var \Symfony\Component\DomCrawler\Crawler */
-      $searchCrawler = $crawler->filter($group['selector']);
+    ] as $selector => $attachments) {
 
-      if (count($searchCrawler) < 1) {
-        continue;
-      }
+      $result->addAttachments($attachments);
 
-      $result->addAttachments($group['attachments']);
     }
 
     return $result;
+
   }
 
 }
