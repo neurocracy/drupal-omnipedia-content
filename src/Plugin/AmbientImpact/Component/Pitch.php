fd75999c8096c27be9870cbc78e371a5891d6782
Matei Stanca <i@ambientimpact.com>
2025-10-14 16:21:56 -0400

    Added a <pitch> element for embedding into content to extend styles.

---
+++ src/Plugin/AmbientImpact/Component/Pitch.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\omnipedia_content\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Omnipedia pitch component.
+ *
+ * @Component(
+ *   id           = "pitch",
+ *   title        = @Translation("Omnipedia pitch"),
+ *   description  = @Translation("Provides a library to extend for pitch styling.")
+ * )
+ */
+class Pitch extends ComponentBase {
+}
